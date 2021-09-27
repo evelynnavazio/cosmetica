a15b1a4fe328bd06b426fe34c59dfe3113a2c7f3
Evelyn Navazio <graueve@hotmail.com>
2021-09-27 10:01:12 -0300

    Entrega 12

--- jquery.js
+++ jquery.js
@@ -1,12 +1,20 @@
-
-$(document).on('ready',function (){
-    
+$(() => {
+    console.log('El DOM esta listo');
 });
 
 
-$('body').append("<p><h2>Escondeme</h2></p>");
+$('#form').on('submit',function(e){
+    e.preventDefault()
+    console.log($("#nombre").val())
+    console.log($("#tel").val())
+    console.log($("#mensaje").val())
+})
 
 
-$('#btnjQuery').on('dblclick', () => {
-    console.log("Respuesta al doble click");
-});
+$('#btnjQuery').on('click', () => {
+    console.log("Respuesta al click");
+})
+
+
+
+
