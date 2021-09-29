5e53fb68f913bf2491b1454de044dc2fdab1a23f
Evelyn Navazio <graueve@hotmail.com>
2021-09-29 19:40:59 -0300

    jquery

--- jquery.js
+++ jquery.js
@@ -2,19 +2,43 @@ $(() => {
     console.log('El DOM esta listo');
 });
 
-
+/*Formulario*/
 $('#form').on('submit',function(e){
     e.preventDefault()
     console.log($("#nombre").val())
     console.log($("#tel").val())
     console.log($("#mensaje").val())
+    console.log($("#email").val())
+    alert("Envio exitoso!")
 })
 
+$("#texto-oculto").prepend('<h3  style="display: none" >Mas sobre cosmética</h3>');
 
+/*show*/
 $('#btnjQuery').on('click', () => {
-    console.log("Respuesta al click");
+    $("h3").show(); 
 })
 
+/*hide*/
+$('#btnjQuery2').on('click', () => {
+    $(".ver-menos").hide();  
+})
 
-
-
+//Declaramos la url del archivo JSON local
+const URLJSON = "users.json"
+//Agregamos un botón con jQuery
+$("body").prepend('<button id="btn1">JSON</button>');
+//Escuchamos el evento click del botón agregado
+$("#btn1").on('click',() => { 
+$.getJSON(URLJSON, function (respuesta, estado) {
+    if(estado === "success"){
+      let misDatos = respuesta;
+      for (const dato of misDatos) {
+        $("body").prepend(`<div>
+                                <h3>${dato.name}</h3>
+                                <p> ${dato.email}</p>
+                            </div>`)
+      }  
+    }
+    });
+});
