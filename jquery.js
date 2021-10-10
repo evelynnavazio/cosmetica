6a120bd7aabbc5910806adea6253edfba0933405
Evelyn Navazio <graueve@hotmail.com>
2021-10-10 20:47:51 -0300

    efectos

--- jquery.js
+++ jquery.js
@@ -1,6 +1,5 @@
 $(() => {
     console.log('El DOM esta listo');
-});
 
 /*Formulario*/
 $('#form').on('submit',function(e){
@@ -9,41 +8,19 @@ $('#form').on('submit',function(e){
     console.log($("#tel").val())
     console.log($("#mensaje").val())
     console.log($("#email").val())
-    alert("Envio exitoso!")
 })
+$( ".ver-mas" ).on('click',function() {
+    $( ".img-1" ).slideToggle( "slow" );
+  });
 
-$("#texto-oculto").prepend('<h3  style="display: none" >Mas sobre cosmética</h3>');
-
-/*show*/
-
-
-/*hide*/
-$('#btnjQuery2').on('click', () => {
-    $(".ver-menos").hide();  
 })
 
 
-const URLJSON = "users.json"
-
-$(".json").prepend('<button id="btn1">JSON</button>');
-
-$("#btn1").on('click',() => { 
-$.getJSON(URLJSON, function (respuesta, estado) {
-    if(estado === "success"){
-      let misDatos = respuesta;
-      for (const dato of misDatos) {
-        $(".json").prepend(`<div>
-                                <h3>Nombre: ${dato.name}</h3>
-                                <p>Email:${dato.email}</p>
-                            </div>`)
-      }  
-    }
-    });
-});
 
 
 
 
-$('#btnjQuery').on('click', () => {
-    $(".ver-menos").toggle(); 
-})
+
+ 
+
+
